4f9a6a8d3a42eea1703154e2118c65be4ea92d3f
Ahmed Samir <ahmed.samir@basharsoft.com>
2013-02-15 02:24:27 +0200

    commiting

--- application/controllers/application.php
+++ application/controllers/application.php
@@ -73,7 +73,7 @@ class Application extends CI_Controller {
     }
     
     public function registration(){
-        $json = $_POST['json'];
+        $json = stripslashes($_POST);
         $decoded = json_decode($json, TRUE);
         
         $data = array();
