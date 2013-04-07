41c037b1b68242b2e190313f04c8ee2d771a74a4
Ahmed Samir <ahmed.samir@basharsoft.com>
2013-04-07 22:14:28 +0200

    commiting

--- application/models/ShopsTable.php
+++ application/models/ShopsTable.php
@@ -19,7 +19,7 @@ class ShopsTable extends Doctrine_Table
     
     public static function getShops(){
         return Doctrine_Query::create()
-                ->select('s.*, op.*, p.*, c.name, m.id, b.name, b.longitude, b.latitude, b.city, b.tel1, b.tel2, b.fax, b.email, b.main_img, col.name, pc.*, cf.*, bf.*, sbf.*')
+                ->select('s.*, op.*, p.*, c.name, m.id, b.name, b.longitude, b.latitude, b.city, b.tel1, b.tel2, b.fax, b.email, b.main_img, col.name, pc.*, cf.*, bf.*, sbf.*, o.*, oc.*')
                 ->from('Shops s, s.LookupShopCategories c, s.ShopMenuSubs m, s.ShopOnlinePresence op')
                 ->leftJoin('m.ShopProducts p ON p.sub_id=m.id')
                 ->leftJoin('m.ShopBranches b ON b.menu_id=m.id')
@@ -27,6 +27,8 @@ class ShopsTable extends Doctrine_Table
                 ->leftJoin('b.BranchFilters bf, bf.ShopCategoryFilters sbf')
                 ->leftJoin('p.ShopProductComponents pc')
                 ->leftJoin('p.ShopCategoryFilters cf')
+                ->leftJoin('m.ShopOffers o')
+                ->leftJoin('o.ShopOfferComponents oc')
                 ->where('s.deleted=0')
                 ->setHydrationMode(Doctrine_Core::HYDRATE_ARRAY)
                 ->execute();
