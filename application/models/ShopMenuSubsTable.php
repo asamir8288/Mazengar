efe2a18bc23be0b8bc5242400e90ebc116af0d00
Ahmed Samir <ahmed.samir@basharsoft.com>
2013-05-12 00:50:34 +0200

    handle category issue

--- application/models/ShopMenuSubsTable.php
+++ application/models/ShopMenuSubsTable.php
@@ -186,7 +186,7 @@ class ShopMenuSubsTable extends Doctrine_Table {
             $n = Doctrine_Query::create()
                     ->select('m.id, m.level, name')
                     ->from('ShopMenuSubs m')
-                    ->where('m.id=?', $q['related_to'])
+                    ->where('m.id=?', ($q['related_to'] - $i))
                     ->setHydrationMode(Doctrine_Core::HYDRATE_ARRAY)
                     ->fetchOne();
 
